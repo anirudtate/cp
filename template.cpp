d0ca438b306525ef63d3beb37d6b590109ab62d3
Anirud Tate <anirud.programming@gmail.com>
2025-04-28 00:28:57 +0530

    updated template

---
+++ template.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define dbg(...) 8
+#endif
+
+using ll = long long;
+
+void solve() {
+    int n;
+    cin >> n;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
